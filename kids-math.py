8e016ab82acd2c6f1862b19cc4926f458c9b2a74
RA <rajiva@cisco.com>
2018-05-01 18:33:34 -0400

    This version provide score summary (if 100% achieves) after each test attempt.

--- kids-math.py
+++ kids-math.py
@@ -10,12 +10,13 @@ import re
 
 # This program is intended to help kids practice with 3 types of math exercises:
 #               - addition, subtraction, multiplication
-# This version is hardcoded for certain names, 20 questions per exercise type
-# and using SQLITE DataBase with pandas package (since it gives us a much faster
-# way to create tables using DataFrame that can be written into DB).
+# This version is hardcoded for certain student names, 20 questions per test per exercise type, and
+# using SQLITE DataBase with pandas package (since it gives us a much faster way to create tables
+# using DataFrame that can be written into DataBase).
 
 # ------changelog -------
-# v7 fixes the second bug that occurred due to copy paste error for counting multiplication tests
+# v8 enhances check_last_performance function to show score per attempt, if 100%
+# v7 fixes the second bug that occurred due to a copy paste error for counting multiplication tests
 # v6 fixes the very first bug that occurred due to functions that got introduced in v5
 # v5 introduces 2 functions for testing and checking past performance for better reusuability
 # v5 also reminds how many tests already done today by a given user and prompts to continue until user says No
@@ -28,15 +29,16 @@ import re
 names = ['Rishik', 'Ria', 'Rajiv', 'Richa', 'Raghav', 'Aadya', 'Ratnabh', 'Ritisha', 'Veda', 'Om']
 calculation = ['nil','addition','subtraction','multiplication']
 
-
 def check_last_performance(name):
     """This function connects to the sql DB and reads the pertinent data from table for further analysis.
     If it is the first test of the day, then print encouraging messages only if 2 or more 100% were obtained
     per calculation in yesterday's performance. If it is not the first test of the day, then print how many tests
      left for the day.
 
-     TODO - This function needs check the all the rows only for the last day instead of all 10 rows.
+     TODO - This function should check all the rows only for the last day instead of last 20 rows.
+     TODO - This should be a fruitful function (instead of a procedure, which returns nothing)
       """
+    x1 = x2 = x3 = y1 = y2 = y3 = z1 = z2 = z3 = 0
 
     with sqlite3.connect('kids-test.db') as conn:
         try:                                                    #do all of the below unless any failure
@@ -49,37 +51,51 @@ def check_last_performance(name):
             yesterdate = (pd.to_datetime('Today') - pd.Timedelta('1 days')).strftime("%Y-%m-%d")
             #print(type(yesterdate))                            #it should be string type, but let's check
 
+            for index, row in df2.iterrows():                       # iterrow() returns a series, let's check row by row
+                if row['calculation'] == calculation[1]:            # note that iterrow() doesnt preserve dtypes across rows
+                    if row['score'] == "100.00":                    # 100.00 is no longer an integer; see comment above
+                        if row['date'] == Todate:
+                            x1 = x1 + 1
+                        elif row['date'] == yesterdate:
+                            x2 = x2 + 1
+                        else:
+                            x3 = x3 + 1
+                elif row['calculation'] == calculation[2]:
+                    if row['score'] == "100.00":
+                        if row['date'] == Todate:
+                            y1 = y1 + 1
+                        elif row['date'] == yesterdate:
+                            y2 = y2 + 1
+                        else:
+                            y3 = y3 + 1
+                elif row['calculation'] == calculation[3]:
+                    if row['score'] == "100.00":
+                        if row['date'] == Todate:
+                            z1 = z1 + 1
+                        elif row['date'] == yesterdate:
+                            z2 = z2 + 1
+                        else:
+                            z3 = z3 + 1
+                else:
+                    print("OUT")
+
             if all(df2.date != Todate):             #For first test today, date would match none of last 20 entries' dates
                 #print(df2[0:2].values)                                 # print values of first 2 rows of dataframe
                 if any(df2.date == yesterdate):
-                    print("Great, I remember you finishing your math yesterday. Good job.")
+                    print("\nGreat, I remember you finishing your math yesterday. Good job.")
+                    if x2 >= 2 and y2 >= 2 or z2 >= 1:
+                        print("###########################################")
+                        print("{}, You rocked with 100% score yesterday. Very impressive. ".format(name))
+                        print("###########################################")
+                        print("Ready to rock again?")
+                    else:
+                        print("###########################################")
+                        print("{}, You were close to rocking with 100% score yesterday. You gotta rock today. You ready?".format(name))
+                        print("###########################################")
+
+
                 else:
                     print("Oh, {} I missed you yesterday. Please dont forget me again.".format(name))
-
-                x=y=z=0
-
-                #Logic Issue here - this for loop checks for 100% score over last many days, instead of just yesterday
-
-                for index, row in df2.iterrows():                           #iterrow() returns a series, let's check row by row
-                    #print(row)
-                    if row['calculation'] == calculation[1]:                #note that iterrow() doesnt preserve dtypes across rows
-                        if row['score'] == "100.00":                        #100.00 is no longer an integer; see comment above
-                            x = x + 1
-                    elif row['calculation'] == calculation[2]:
-                        if row['score'] == "100.00":
-                            y = y + 1
-                    elif row['calculation'] == calculation[3]:
-                        if row['score'] == "100.00":
-                            z = z + 1
-                if x >= 2 and y >=2 or z >=1:
-                    print("###########################################")
-                    print("{}, You rocked with 100% score last time. Very impressive. ".format(name))
-                    print("###########################################")
-                    print("Ready to rock again?")
-                else:
-                    print("###########################################")
-                    print("{}, You gotta score 100% in at least 2 tests today. You ready?".format(name))
-                    print("###########################################")
 
             else:                                                       #if it is not the first test of the day, then find test taken so far
                 #count number of rows that match test type e.g. addition and that match today's date, match on date first to minimize
@@ -89,15 +105,18 @@ def check_last_performance(name):
                 mul_test_no = len(df2[(df2['date'] == Todate) & (df2['calculation'] == 'multiplication')])
 
                 print("{}, Good that you want to continue. So far, You have finished::".format(name))
-                print("\t {} of 3 additions,".format(add_test_no))
-                print("\t {} of 3 substractions,".format(sub_test_no))
-                print("\t {} of 3 multiplications,".format(mul_test_no))
+                print("\t {} of 3 additions with 100% score in {} of them,".format(add_test_no, x1))
+                print("\t {} of 3 substractions with 100% score in {} of them,".format(sub_test_no, y1))
+                print("\t {} of 3 multiplications with 100% score in {} of them,".format(mul_test_no, z1))
 
-                if add_test_no == 3 and sub_test_no == 3 and mul_test_no ==3:
+                if add_test_no == 3 and sub_test_no == 3 and mul_test_no == 3:
                     print("You are done already. Feel free to do something else and Have fun.")
+                    ans = input('\n{}, Do you want to quit (y/n): '.format(name))
+                    if ans == 'y' or 'yes' or 'Y':
+                        exit()
+
                 else:
                     print("You are doing great. Now, finish 3 or more of each quickly.")
-
 
 
         except (RuntimeError, TypeError, NameError):
@@ -119,7 +138,7 @@ def math_test(name):
         range2 = 10
     else:
         range1 = 5
-        range2 = 40
+        range2 = 30
 
     startGameTime = time.time()
     for x in range(2):                                              # Number of questions is 20
@@ -156,7 +175,7 @@ def math_test(name):
                     except ValueError:                              # blank or alphabets not allowed, go back
                         print('Please enter a valid number')
 
-        elif int(math)==3 :                                           # if multiplication is chosen
+        elif int(math) == 3:                                           # if multiplication is chosen
             x1 = random.randint(2, 4)                                 # let's focus on table of 2 or 3
             y1 = random.randint(0, 11)
 
@@ -171,6 +190,7 @@ def math_test(name):
         else:
             print('sorry, you should have chosen 1 or 2 or 3. Try again')
             exit()
+
 
         endTime = time.time()
         if (z == z1):                                                 # if correct answer
@@ -244,7 +264,7 @@ while True:
     #progress only if name is already in the list, otherwise keep asking
     if name in names:
 
-        # keep testing until student wants to quick by not typing 'y'
+        # keep testing until student wants to quit by not typing 'y'
         while True:
             # Let's find out about the last performance per calculation and encourage before test starts
             check_last_performance(name)
