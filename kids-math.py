80d7009bec4ce61c97d54d89f7901555f57d6ec5
Rajiv Asati <rajiva@cisco.com>
2018-08-04 15:28:55 -0400

    TimeZone Adjustment
    This version allows the program to run in any timezone, while keeping
    only Eastern Timezone in SQLDB (remember the program also checks for
    prior performance, hence, date/time consistency is key)

--- kids-math.py
+++ kids-math.py
@@ -2,8 +2,9 @@
 
 import numpy as np
 import random
-import datetime
 import time
+from datetime import datetime, timedelta
+from pytz import timezone
 import pandas as pd
 import sqlite3
 import re
@@ -15,6 +16,7 @@ import re
 # using DataFrame that can be written into DataBase).
 
 # ------changelog -------
+# v9 adjusts the date & time to Eastern timezone even if server time is set to another timezone
 # v8 enhances check_last_performance function to show score per attempt, if 100%
 # v7 fixes the second bug that occurred due to a copy paste error for counting multiplication tests
 # v6 fixes the very first bug that occurred due to functions that got introduced in v5
@@ -27,7 +29,8 @@ import re
 
 
 names = ['Rishik', 'Ria', 'Rajiv', 'Richa', 'Raghav', 'Aadya', 'Ratnabh', 'Ritisha', 'Veda', 'Om']
-calculation = ['nil','addition','subtraction','multiplication']
+calculation = ['nil','addition','subtraction','multiplication', 'division']
+
 
 def check_last_performance(name):
     """This function connects to the sql DB and reads the pertinent data from table for further analysis.
@@ -38,18 +41,32 @@ def check_last_performance(name):
      TODO - This function should check all the rows only for the last day instead of last 20 rows.
      TODO - This should be a fruitful function (instead of a procedure, which returns nothing)
       """
-    x1 = x2 = x3 = y1 = y2 = y3 = z1 = z2 = z3 = 0
+    x1 = x2 = x3 = y1 = y2 = y3 = z1 = z2 = z3 = zz1 = zz2 = zz3 = 0
 
     with sqlite3.connect('kids-test.db') as conn:
-        try:                                                    #do all of the below unless any failure
+        """do all of the below (under try clause) unless any failure"""
+
+        try:
+
             df2 = pd.read_sql('SELECT attempt, date, calculation, score, TotalTime from Math WHERE name=?', conn, params=[name], index_col='attempt')
 
             df2 = df2.tail(20)                                   #last 20 rows should have all of last time's performance
-            #print(df2.loc[data['date'].str.contains('2018-03-17')])
+            #print(df2.loc[data['date'].str.contains('2018-03-17')])    # check if there is any row for a particular date
 
-            Todate = time.strftime("%Y-%m-%d")
-            yesterdate = (pd.to_datetime('Today') - pd.Timedelta('1 days')).strftime("%Y-%m-%d")
-            #print(type(yesterdate))                            #it should be string type, but let's check
+            # if the server date/time is not in Eastern timezone (value 18000), then convert it
+            if time.timezone != 18000:
+                now_utc = datetime.now(timezone('UTC'))
+                now_ET = now_utc.astimezone(timezone('US/Eastern'))
+                yesterday_utc = datetime.now(timezone('UTC')) - timedelta(days=1)
+                yesterday_ET = yesterday_utc.astimezone(timezone('US/Eastern'))
+                Todate = now_ET.strftime("%Y-%m-%d")
+                yesterdate = yesterday_ET.strftime("%Y-%m-%d")
+            else:
+                Todate = time.strftime("%Y-%m-%d")
+                yesterdate = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+                print(Todate)
+
+            #print(type(yesterdate))                                #it should be string type, but let's check
 
             for index, row in df2.iterrows():                       # iterrow() returns a series, let's check row by row
                 if row['calculation'] == calculation[1]:            # note that iterrow() doesnt preserve dtypes across rows
@@ -76,18 +93,27 @@ def check_last_performance(name):
                             z2 = z2 + 1
                         else:
                             z3 = z3 + 1
+                elif row['calculation'] == calculation[4]:
+                    if row['score'] == "100.00":
+                        if row['date'] == Todate:
+                            zz1 = zz1 + 1
+                        elif row['date'] == yesterdate:
+                            zz2 = zz2 + 1
+                        else:
+                            zz3 = zz3 + 1
                 else:
                     print("OUT")
 
-            if all(df2.date != Todate):             #For first test today, date would match none of last 20 entries' dates
+            if all(df2.date != Todate):             #For first test today, Today's date would match none of last 20 entries' dates
                 #print(df2[0:2].values)                                 # print values of first 2 rows of dataframe
                 if any(df2.date == yesterdate):
-                    print("\nGreat, I remember you finishing your math yesterday. Good job.")
-                    if x2 >= 2 and y2 >= 2 or z2 >= 1:
+                    print("\n {}, I remember you finishing your math yesterday. Good job.".format(name))
+                    if x2 >= 2 and y2 >= 2 or z2 >= 1 or zz2 >= 1:
                         print("###########################################")
-                        print("{}, You rocked with 100% score yesterday. Very impressive. ".format(name))
+                        print("And You rocked with 100% score yesterday. Very impressive, {}.".format(name))
                         print("###########################################")
                         print("Ready to rock again?")
+                        return(1)
                     else:
                         print("###########################################")
                         print("{}, You were close to rocking with 100% score yesterday. You gotta rock today. You ready?".format(name))
@@ -103,13 +129,15 @@ def check_last_performance(name):
                 add_test_no = len(df2[(df2['date'] == Todate) & (df2['calculation'] == 'addition')])
                 sub_test_no = len(df2.loc[(df2['date'] == Todate) & (df2['calculation'] == 'subtraction')])    #this also works
                 mul_test_no = len(df2[(df2['date'] == Todate) & (df2['calculation'] == 'multiplication')])
+                div_test_no = len(df2[(df2['date'] == Todate) & (df2['calculation'] == 'division')])
 
                 print("{}, Good that you want to continue. So far, You have finished::".format(name))
                 print("\t {} of 3 additions with 100% score in {} of them,".format(add_test_no, x1))
-                print("\t {} of 3 substractions with 100% score in {} of them,".format(sub_test_no, y1))
+                print("\t {} of 3 subtractions with 100% score in {} of them,".format(sub_test_no, y1))
                 print("\t {} of 3 multiplications with 100% score in {} of them,".format(mul_test_no, z1))
+                print("\t {} of 3 Divisions with 100% score in {} of them,".format(mul_test_no, z1))
 
-                if add_test_no == 3 and sub_test_no == 3 and mul_test_no == 3:
+                if add_test_no == 3 and sub_test_no == 3 and mul_test_no == 3 and div_test_no == 3:
                     print("You are done already. Feel free to do something else and Have fun.")
                     ans = input('\n{}, Do you want to quit (y/n): '.format(name))
                     if ans == 'y' or 'yes' or 'Y':
@@ -117,35 +145,39 @@ def check_last_performance(name):
 
                 else:
                     print("You are doing great. Now, finish 3 or more of each quickly.")
+                    return(0)
 
 
         except (RuntimeError, TypeError, NameError):
+            print('Something gone wrong with last_check_performance')
             print('Learn more about error handling here - https://docs.python.org/3/tutorial/errors.html')
         except:
             print('Oops, DB didnt have Math table, or table didnt have a column, or something went wrong in logic')
 
 
-def math_test(name):
+def math_test(name, flag):
     # Ask to select math type
-    math = input('\n Alright {}, Press 1 for Addition, 2 for Subtraction, 3 for Multiplication: '.format(name))
+    math = input('\n Alright {}, Press 1 for Addition, 2 for Subtraction, 3 for Multiplication, 4 for Division: '.format(name))
 
     a = b = z = 0
     maxT = minT = 60
 
-    if name == names[1]:  # If one needs to practice for smaller numbers
-        # print('{}, You did great yesterday..'.format(name))
+    if name == names[1]:   #If one needs to practice for smaller numbers, then change the range
         range1 = 1
         range2 = 10
     else:
         range1 = 5
-        range2 = 30
+        range2 = 50
+        if flag == 1:
+            print("Flag is {},".format(flag))
+            range1 = range1 + 1
+            range2 = range2 + 10
 
     startGameTime = time.time()
     for x in range(2):                                              # Number of questions is 20
         x1 = random.randint(range1,range2)
         y1 = random.randint(range1,range2)
         startTime = time.time()
-        print('\n')
 
         if int(math)==1 :                                             # if addition is chosen
             z1 = x1 + y1
@@ -176,7 +208,7 @@ def math_test(name):
                         print('Please enter a valid number')
 
         elif int(math) == 3:                                           # if multiplication is chosen
-            x1 = random.randint(2, 4)                                 # let's focus on table of 2 or 3
+            x1 = random.randint(2, 6)                                 # let's focus on table of 2 or 3
             y1 = random.randint(0, 11)
 
             z1 = x1 * y1
@@ -187,10 +219,28 @@ def math_test(name):
                 except ValueError:
                     print('Please enter a valid number')
 
-        else:
-            print('sorry, you should have chosen 1 or 2 or 3. Try again')
-            exit()
+        elif int(math) == 4:                                           # In division, bigger number is divided by smaller number
+            if x1 >= y1 :
+                z1 = x1 / y1
+                while True:
+                    try:
+                        z = int(input('{})  {} / {} = '.format(x + 1, x1, y1)))
+                        break                                        # if a valid integer is entered, then exit
+                    except (ValueError):                             # blank or alphabets not allowed, go back
+                        print('Please enter a valid number')
 
+            elif y1 > x1 :
+                z1 = y1 / x1
+                while True:
+                    try:
+                        z = int(input('{})  {} / {} = '.format(x + 1, y1, x1)))
+                        break
+                    except ValueError:                              # blank or alphabets not allowed, go back
+                        print('Please enter a valid number')
+
+        else:
+            print('sorry, you should have chosen 1 or 2 or 3 or 4. Try again')
+            exit()
 
         endTime = time.time()
         if (z == z1):                                                 # if correct answer
@@ -226,17 +276,31 @@ def math_test(name):
               .format(Percent, a, b, round(endGameTime-startGameTime)))
     print('\t Your fastest time was {} sec & slowest time was {} sec'.format(minT,maxT))
 
-    # store all the info in a pandas dataframe row, which can later be written into a SQL DB
-    # Note that a list requires an index to be an integer, not a string, hence, int(math) is used with calculation
+    # if the local time not in Eastern timezone (value 18000), then convert it
+    if time.timezone != 18000:
+        now_utc = datetime.now(timezone('UTC'))
+        now_ET = now_utc.astimezone(timezone('US/Eastern'))
+        ET_date = now_ET.strftime("%Y-%m-%d")
+        ET_time = now_ET.strftime("%X")
+    else:
+        ET_date = time.strftime("%Y-%m-%d")
+        ET_time = time.strftime("%X")
 
-    df = pd.DataFrame([[name, 1, time.strftime("%Y-%m-%d"), time.strftime("%X"), calculation[int(math)], a + b, Percent, minT, maxT, round(endGameTime - startGameTime)]],
+    """ store all the info in a pandas dataframe row, which can later be written into a SQL DB
+     Note that a list requires an index to be an integer, not a string, hence, int(math) is used with calculation
+    """
+
+    df = pd.DataFrame([[name, 1, ET_date, ET_time, calculation[int(math)], a + b, Percent, minT, maxT, round(endGameTime - startGameTime)]],
           columns=['name', 'attempt', 'date', 'time', 'calculation', 'totalQ', 'score', 'bestTime', 'worstTime', 'TotalTime'])
+
     #print(df)
     #print(df.describe())                                                               #prints std,50% values
 
-    # open sql database and check for the Math table to be present
-    # find the last attempt number for a given user name and then replace it with the current one
-    # write the pandas dataframe into the table
+    """
+    Now, let's save the dataframe in a table, but let's first check for that table to be present in DB
+    find the last attempt number for a given user name and then replace it with the current one
+    write the pandas dataframe into the table
+    """
 
     with sqlite3.connect('kids-test.db') as conn:
         try:                                                                            # proceed if Math table existed in DB
@@ -251,24 +315,28 @@ def math_test(name):
             print('Learn more about error handling here - https://docs.python.org/3/tutorial/errors.html')
         except:
             print('Oops, either DB didnt have Math table, or something went wrong in logic')
-                                                                                        # write the dataframe into Math table
+
+
+        """ All checked. Let's write the dataframe into Math table """
         df.to_sql('Math', conn, if_exists='append',index=False, index_label=['name','attempt','date', 'time','calculation','totalQ','score','bestTime','worstTime','TotalTime'])
 
 
-#keep on asking for the name until it is entered exactly as expected by using 'while true' and break
-#only if matching name is entered, but not do the same in case of math type being not 1 or 2 or 3.
+"""keep on asking for the name until it is entered exactly as expected by using 'while true' and break
+only if matching name is entered, but not do the same in case of math type being not 1 or 2 or 3 or 4
+"""
 
 while True:
     name = input('Welcome, Enter your name: ')                      #input() always returns a string
 
-    #progress only if name is already in the list, otherwise keep asking
+    """progress only if name is already in the list, otherwise keep asking"""
     if name in names:
 
-        # keep testing until student wants to quit by not typing 'y'
+        """ keep testing until student wants to quit by not typing 'y'. """
         while True:
             # Let's find out about the last performance per calculation and encourage before test starts
-            check_last_performance(name)
-            math_test(name)
+            flag = 0
+            flag = check_last_performance(name)
+            math_test(name, flag)
             ans = input('\n{}, Do you want to continue (y/n): '.format(name))
             if ans != 'y':
                 break
